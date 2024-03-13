e36476ea4ee91d68093d23f6382f0ef79f57e4fa
Paulo Zanetti <paulozanetti15@hotmail.com>
2024-03-13 19:37:50 -0300

    Ex03 - updated

---
+++ src/main/kotlin/Pessoa.kt
@@ -0,0 +1,18 @@
+//3. Crie uma classe chamada "Pessoa" com os atributos "nome" e "idade". Em seguida, crie uma lista de objetos "Pessoa" e ordene a lista em ordem alfabética pelo atributo "nome"
+
+class Pessoa(val nome: String, val idade: Int)
+
+fun main() {
+    val pessoas = listOf(
+        Pessoa("Pz", 24),
+        Pessoa("Fasola", 20),
+        Pessoa("Lucato", 35)
+    )
+
+    val pessoasOrdenadas = pessoas.sortedBy { it.nome }
+
+    println("Lista de pessoas ordenada por nome:")
+    pessoasOrdenadas.forEach { pessoa ->
+        println("Nome: ${pessoa.nome}, Idade: ${pessoa.idade}")
+    }
+}
